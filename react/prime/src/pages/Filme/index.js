0fe526abe35a2d38370a7a928ce3c8c9236ecdbc
Lucas Ramos <lucas.bruneli.ramos@gmail.com>
2022-06-27 20:37:17 -0300

    update 27/06/22

--- react/prime/src/pages/Filme/index.js
+++ react/prime/src/pages/Filme/index.js
@@ -1,10 +1,15 @@
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
 import api from '../../services/api'
+import './filme-info.css'
 
 function Filme(){
   const { id } = useParams()
+  const navigate = useNavigate()
+  const [filme, setFilme] = useState({})
+  const [loading, setLoading] = useState(true)
+  
 
   useEffect(()=>{
 
@@ -16,20 +21,71 @@ function Filme(){
         }
       })
       .then((response)=>{
-        console.log(response)
+        //console.log(response.data)
+        setFilme(response.data)
+        setLoading(false)
       })
       .catch(()=>{
-        console.log("Filme Não encontrado")
+        //console.log("Filme Não encontrado")
+        navigate("/", { replace: true })
+        return
       })
     }
 
     loadFilme()
 
-  }, [])
+    return () => {
+      //console.log("COMPONENTE FOI DESMONTADO")
+    }
+
+  }, [navigate, id])
+
+  function salvarFilme() {
+    const minhaLista = localStorage.getItem("@primeFlix")
+
+    let filmesSalvos = JSON.parse(minhaLista) || [];
+
+    const hasFilme = filmesSalvos.some((filmesSalvo) => filmesSalvo.id === filme.id)
+    //console.log(hasFilme)
+
+    if(hasFilme) {
+      alert("ESSE FILME JA EXISTE!")
+      return
+    }
+
+    filmesSalvos.push(filme)
+    localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos))
+    alert("FILME SALVO COM SUCESSO")
+
+  }
+
+  if(loading) {
+    return(
+      <div className='filme-info'>
+        <h1>Carregando detalhes...</h1>
+      </div>
+    )
+  }
 
   return(
-    <div>
-      <h1>ACESSANDO O FILME {id}</h1>
+    <div className='filme-info'>
+      <h1>{filme.title}</h1>
+      <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}  />
+    
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+
+      <strong>Avaliação: {filme.vote_average} / 10</strong>
+      
+      <div className='area-buttons'>
+        <button onClick={salvarFilme}>Salvar</button>
+        <button>
+          <a target="blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
+            Trailer
+          </a>
+        </button>
+      </div>
+
     </div>
   )
 }
